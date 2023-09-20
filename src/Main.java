4d56af5cf49100188147374f0e52147d937d5a97
FrenzyG7 <rubenverrod@gmail.com>
2023-09-20 21:48:10 +0200

    what I think is correct

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,26 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class Main {
-    public static File fileRoute = new File("./quijote.txt");
-    public static StringBuilder textFile;
 
     public static void main(String[] args){
-        timesInFileCharacter('a');
-        compareAppearancesOfCharacterInFile();
+        timesInFileCharacter('a', new File("./quijote.txt"));
+        compareAppearancesOfCharacterInFile(new File("./quijote.txt"));
     }
 
-    public static int timesInFileCharacter(char givenCharacter) { //TODO file parametro
+    public static int timesInFileCharacter(char givenCharacter, File givenFile) {
         int charCounter = 0;
-        for (int i = 0; i < Main.textFile.length(); i++) {
-            if (Main.textFile.charAt(i) == givenCharacter) {
+        StringBuilder textFile = new StringBuilder();
+        Scanner textReader = null;
+        try {
+            textReader = new Scanner(givenFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (textReader.hasNext()) {
+            textFile.append(textReader.nextLine());
+        }
+        for (int i = 0; i < textFile.length(); i++) {
+            if (textFile.charAt(i) == givenCharacter) {
                 charCounter++;
             }
         }
@@ -23,15 +31,25 @@ public class Main {
 
     }
 
-    public static void compareAppearancesOfCharacterInFile() {
+    public static void compareAppearancesOfCharacterInFile(File givenFile) {
         Map<Character, Integer> map;
         map = new HashMap<Character, Integer>();
-        for (int i = 0; i < Main.textFile.length(); i++) {
-            if (!map.containsKey(Main.textFile.charAt(i))){
-                map.putIfAbsent(Main.textFile.charAt(i), map.getOrDefault(Main.textFile.charAt(i), 0)+1);
+        StringBuilder textFile = new StringBuilder();
+        Scanner textReader = null;
+        try {
+            textReader = new Scanner(givenFile);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (textReader.hasNext()) {
+            textFile.append(textReader.nextLine());
+        }
+        for (int i = 0; i < textFile.length(); i++) {
+            if (!map.containsKey(textFile.charAt(i))){
+                map.putIfAbsent(textFile.charAt(i), map.getOrDefault(textFile.charAt(i), 0)+1);
             }
             else {
-                map.put(Main.textFile.charAt(i), map.get(Main.textFile.charAt(i))+1);
+                map.put(textFile.charAt(i), map.get(textFile.charAt(i))+1);
             }
         }
         int maxValue = Collections.max(map.values());
@@ -44,20 +62,7 @@ public class Main {
         System.out.println("El caracter más usado es: " + key + "\nCon: " + maxValue + " usos"); //El caracter más usado es espacio, yo no hago las reglas
     }
 
-    public static StringBuilder fileStore() throws FileNotFoundException {
-        StringBuilder textFile = new StringBuilder();
-        Scanner textReader = new Scanner(Main.fileRoute);
-        while (textReader.hasNext()) {
-            textFile.append(textReader.nextLine());
-        }
-        return textFile;
-    }
 
-    static {
-        try {
-            Main.textFile = fileStore();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
+
 }
